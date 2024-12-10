b86a53e2ae050ed5f3919ed26a5fa582bee85e90
darmali <waleeddarmali@gmail.com>
2024-12-10 16:27:24 +0300

    add mapview for hiveboard app

--- HiveBoard/app/index.tsx
+++ HiveBoard/app/index.tsx
@@ -1,13 +1,28 @@
-import { View, Text, FlatList, useWindowDimensions, ActivityIndicator } from "react-native";
+import { View, Text, FlatList, useWindowDimensions, ActivityIndicator, StyleSheet } from "react-native";
 //import products from '../assets/products.json'
 import ProductListItem from '../components/ProductListItem'
 import { useBreakpointValue } from "@/components/ui/utils/use-break-point-value";
 import { listProducts } from "@/api/products";
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 
 export default function HomeScreen() {
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+        },
+        map: {
+            width: '100%',
+            height: '100%',
+        },
+    });
+    const [coordinate, setCoordinate] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+    });
+ 
     const { data, isLoading, error } = useQuery({
         queryKey: ['products'],
         queryFn: listProducts,
@@ -19,10 +34,10 @@ export default function HomeScreen() {
         lg: 3,
         '2xl': 4
     })
-    if(isLoading){
-        return(<ActivityIndicator />);
+    if (isLoading) {
+        return (<ActivityIndicator />);
     }
-    if(error){
+    if (error) {
         return (<Text> Error Fetching Products</Text>);
     }
     return (
@@ -34,5 +49,19 @@ export default function HomeScreen() {
             columnWrapperClassName="gap-2"
             renderItem={({ item }) => <ProductListItem product={item} />}
         />
+        // <View style={styles.container}>
+        //     <MapView style={styles.map}
+        //         //provider={PROVIDER_GOOGLE}
+        //         onPress={(e) => {
+        //             console.log("Coordinates", e.nativeEvent);
+        //             setCoordinate(e.nativeEvent.coordinate);
+        //         }}>
+        //         <Marker
+        //             coordinate={coordinate}
+        //             title="My Location"
+        //             description="This is a marker in San Francisco"
+        //         />
+        //     </MapView>
+        // </View>
     );
 }
